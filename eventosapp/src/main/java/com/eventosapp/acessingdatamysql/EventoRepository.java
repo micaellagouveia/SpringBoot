f1302b7e5f5dbe5bafceccd5f3134c32f782912f
Micaella Gouveia <micaella2212@gmail.com>
2020-03-26 20:55:35 -0300

    Adding EventosApp file

---
+++ eventosapp/src/main/java/com/eventosapp/acessingdatamysql/EventoRepository.java
@@ -0,0 +1,8 @@
+package com.eventosapp.acessingdatamysql;
+
+import com.eventosapp.models.Evento;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventoRepository extends CrudRepository<Evento, Integer> {
+
+}
